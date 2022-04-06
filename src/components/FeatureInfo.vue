1b4fc8cffcd25e806c00be966a7cd8e1ab7d5c7c
Steve Bennett <me@stevebennett.me>
2022-04-06 22:41:18 +1000

    Read expedition story within site

--- src/components/FeatureInfo.vue
+++ src/components/FeatureInfo.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
 #FeatureInfo.pa2(v-if="p")
+    .relative
+        .closebtn.pointer.absolute(@click="feature=null" style="top: -0.25em; right:-0.125em;") ×
     //- img.image(v-if="imageUrl" :src="imageUrl")
     h2.mt0.mb2 {{ p.success ? 'Successful' : 'Failed' }} expedition
 
@@ -21,6 +23,9 @@
     //-         template(v-if="ignoreProps.indexOf(prop) === -1")
     //-             th.f6 {{ prop }}
     //-             td.f6 {{ value }}
+
+    .story.h-5
+        .content(v-html="story")
 </template>
 
 <script>
@@ -30,6 +35,7 @@ export default {
     data: () => ({
         feature: undefined,
         ignoreProps: ['id', 'Longitude', 'Latitude', 'image_url'],
+        story: null,
     }),
     computed: {
         p() {
@@ -44,6 +50,33 @@ export default {
         window.app.FeatureInfo = this;
         EventBus.$on('select-feature', (feature) => (this.feature = feature));
     },
+    watch: {
+        p() {
+            this.story = null;
+            if (this.p?.id) {
+                fetch(
+                    // `https://geohashing.site/api.php?action=parse&page=${this.p.id}&format=json`
+                    `https://fippe-geojson.glitch.me/wiki/?action=parse&page=${this.p.id}&format=json`
+                )
+                    .then((res) => res.json())
+                    .then((res) => {
+                        this.story = res.parse.text['*']
+                            .replace(
+                                /\/geohashing\/File/g,
+                                'https://geohashing.site/geohashing/File'
+                            )
+                            .replace(
+                                /\/geohashing\/Image/g,
+                                'https://geohashing.site/geohashing/Image'
+                            )
+                            .replace(
+                                /"\/images\/thumb\//g,
+                                '"https://geohashing.site/images/thumb/'
+                            );
+                    });
+            }
+        },
+    },
 };
 </script>
 
@@ -54,13 +87,17 @@ export default {
     border: 1px solid #111;
 }
 
-a {
-    color: hsl(240, 50%, 70%);
-    text-decoration: none;
-}
-
 #FeatureInfo th {
     text-align: right;
+}
+
+.closebtn {
+    font-size: 30px;
+}
+
+.closebtn:hover {
+    color: white;
+    font-weight: bold;
 }
 
 .image {
@@ -69,5 +106,72 @@ a {
 
 li {
     list-style-type: none;
+}
+
+.story {
+    overflow-y: scroll;
+    height: 500px;
+}
+</style>
+<style>
+#FeatureInfo a {
+    color: hsl(240, 50%, 70%);
+    text-decoration: none;
+}
+
+.story .mw-parser-output table.infobox {
+    display: none;
+}
+
+.story h2 {
+    font-size: 14px;
+    margin: 0;
+}
+.story h3,
+.story h4 {
+    font-size: 14px;
+    margin: 0;
+}
+
+.story .mw-editsection {
+    display: none;
+}
+
+.story p,
+.story li,
+.story {
+    font-size: 12px;
+}
+
+.story p {
+    margin-top: 0;
+}
+
+.story #toc {
+    display: none;
+}
+
+.story {
+    background: hsl(0, 0%, 15%);
+    padding: 10px;
+}
+
+.story ul {
+    list-style: none;
+    padding: 0;
+}
+
+.story ul.gallery {
+    list-style: none;
+    padding: 0;
+}
+
+.story .gallerybox .thumb div {
+    margin: 0 !important;
+}
+
+.story .gallerybox,
+.story .gallerybox div {
+    width: unset !important;
 }
 </style>
